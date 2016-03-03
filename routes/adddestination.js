77079627a4d747d9b0eb78654681b4ab31b90aac
Gabe Maze-Rogers <gmazerogers@gmail.com>
2016-03-02 23:19:07 -0800

    analytics

--- routes/adddestination.js
+++ routes/adddestination.js
@@ -2,11 +2,11 @@ var destinations = require("../dest.json");
 
 exports.addDestination = function(req, res) { 
 	console.log("hi")
-	
+
     var newDestination = {
     	"name" : req.query.name,
     	"address" : req.query.address
-    	
+
     };
     destinations["destinations"].push(newDestination);
     console.log(newDestination);
